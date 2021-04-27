9692c3275c91cf7db45942cea203faea99091ec1
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-04-27 17:18:57 +0800

    update sia

--- hr_insurance/models/employee_insurance.py
+++ hr_insurance/models/employee_insurance.py
@@ -33,8 +33,8 @@ class EmployeeInsurance(models.Model):
 
     @api.onchange('policy_id')
     def _compute_amount(self):
-        if self.policy_id.insure_type == "ISA":
-            self.company_amount = self.policy_id.company_percentage * self.employee_id.contract_id.isa
+        if self.policy_id.insure_type == "SIA":
+            self.company_amount = self.policy_id.company_percentage * self.employee_id.contract_id.sia
         else:
             self.company_amount = self.policy_id.company_percentage * self.employee_id.contract_id.hra
 
--- hr_insurance/models/policy_details.py
+++ hr_insurance/models/policy_details.py
@@ -9,8 +9,8 @@ class InsurancePolicy(models.Model):
     name = fields.Char(string='Name', required=True, translate=True)
     company_percentage = fields.Float(string="Company Percentage ", help="Company insurance percentage")
     personal_percentage = fields.Float(string="Personal Percentage ", help="Personal insurance percentage")
-    insure_type = fields.Selection([('ISA', 'Social Insurance Base'), ('HRA', 'House Rent Allowance Base')],
-                                       required=True, default='ISA',
+    insure_type = fields.Selection([('SIA', 'Social Insurance Base'), ('HRA', 'House Rent Allowance Base')],
+                                       required=True, default='SIA',
                                        string='Insurance Type', help="Insurance Type")
     note_field = fields.Html(string='Comment', help="Notes for the insurance policy if any")
     company_id = fields.Many2one('res.company', string='Company', required=True, help="Company",
