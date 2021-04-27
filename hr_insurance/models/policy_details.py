e29f89b2eb3a95fa199ffab78ae471741e03256e
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-04-27 17:04:19 +0800

    update

--- hr_insurance/models/policy_details.py
+++ hr_insurance/models/policy_details.py
@@ -10,7 +10,7 @@ class InsurancePolicy(models.Model):
     company_percentage = fields.Float(string="Company Percentage ", help="Company insurance percentage")
     personal_percentage = fields.Float(string="Personal Percentage ", help="Personal insurance percentage")
     insure_type = fields.Selection([('ISA', 'Social Insurance Base'), ('HRA', 'House Rent Allowance Base')],
-                                       required=True, default='ISa',
+                                       required=True, default='ISA',
                                        string='Insurance Type', help="Insurance Type")
     note_field = fields.Html(string='Comment', help="Notes for the insurance policy if any")
     company_id = fields.Many2one('res.company', string='Company', required=True, help="Company",
