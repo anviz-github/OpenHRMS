baa58e243a784a32494a453bbb3634ae81b6cab8
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-05-08 13:47:33 +0800

    add print

--- payroll_kra/models/employee_kra.py
+++ payroll_kra/models/employee_kra.py
@@ -40,6 +40,7 @@ class payroll_kra(models.Model):
     def _kra_final_score_compute(self):
         this_month = datetime.now().month
         year = datetime.now().year
+
         if this_month == 1:
             last_month = 12
             year = year - 1
@@ -49,27 +50,22 @@ class payroll_kra(models.Model):
         for emp in self:
             final_score_this = 0
             final_score_last = 0
-
             this_records = 0
             last_records = 0
+
             for kra_id in emp.kras:
 
-
-
-                if kra_id.name == str(this_month) and kra_id.year.name == str(year):
-                    this_records += 1
-                elif kra_id.name == str(last_month) and kra_id.year.name == str(year):
-                    last_records += 1
-
                 questions = self.env['employee.kra.question'].search([('employee_kra_id', '=', kra_id.id)])
-
+                print('questions', questions)
                 for question in questions:
-                    if kra_id.name == str(this_month):
+                    if kra_id.name == str(this_month) and kra_id.year.name == str(year):
+                        this_records += 1
                         final_score_this = final_score_this + question.final_score
-                    else:
+
+                    elif kra_id.name == str(last_month) and kra_id.year.name == str(year):
+                        last_records += 1
                         final_score_last = final_score_last + question.final_score
 
-           
             if last_records == 0:
                 last_records = 1
             if this_records == 0:
