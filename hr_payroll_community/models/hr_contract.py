174cfd57ac07cade07356ffedcc2c9e450779c99
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-05-12 14:38:49 +0800

    add trial addition and bonus

--- hr_payroll_community/models/hr_contract.py
+++ hr_payroll_community/models/hr_contract.py
@@ -33,8 +33,12 @@ class HrContract(models.Model):
     sia = fields.Monetary(string='SIA', tracking=True, help="Social insurance allowance base.")
     trial_wage = fields.Monetary('Trial Wage', required=True, tracking=True, help="Employee's monthly trial wage.")
     additional_wage = fields.Monetary('Additional Wage', required=True, tracking=True, help="Employee's monthly additional wage.")
+    additional_wage_trial = fields.Monetary('Additional Wage Trial', required=True, tracking=True, help="Employee's monthly additional wage trial.")
+
     performance_bonus = fields.Monetary('Performance Bonus', required=False, tracking=True,
                                       help="Employee's monthly performance bonus.")
+    performance_bonus_trial = fields.Monetary('Performance Bonus Trial', required=False, tracking=True,
+                                        help="Employee's monthly performance bonus trial.")
     def get_all_structures(self):
         """
         @return: the structures linked to the given contracts, ordered by hierachy (parent=False first,
