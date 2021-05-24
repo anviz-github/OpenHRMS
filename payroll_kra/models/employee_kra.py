ed6cf0579ae51ced33b86270dd839a2a699b947b
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-05-24 18:50:08 +0800

    update

--- payroll_kra/models/employee_kra.py
+++ payroll_kra/models/employee_kra.py
@@ -14,15 +14,18 @@ class payroll_kra(models.Model):
     kra_final_score_last_month = fields.Float(compute='_kra_final_score_compute', string='Last Month Final Score' \
                                               , default=100, readonly='1')
     #    , store=True)
-
+    kra_final_score_this_quarterly = fields.Float(compute='_kra_final_score_compute', string='This quarterly  final score', default=100, readonly='1')
+    kra_final_score_last_quarterly = fields.Float(compute='_kra_final_score_compute', string='Last quarterly final score', default=100, readonly='1')
     kras = fields.One2many('employee.kra', 'employee_id', string="KRAS", help="KRAS", compute='_get_kras')
                           #domain=[('state', '=', 'done')])
 
     def _get_kras(self):
         months = []
+        quarterlys = []
         this_month = datetime.now().month
         year = datetime.now().year
         months.append(this_month)
+
         if this_month == 1:
             last_month = 12
             year = year - 1
@@ -30,6 +33,14 @@ class payroll_kra(models.Model):
             last_month = this_month - 1
         months.append(last_month)
 
+        this_quarterly = (this_month - 1)//3 + 1
+        quarterlys.append(this_quarterly)
+        if ((this_quarterly - 1)//3 - 1) == 0:
+            last_quarterly = 1
+            quarterlys.append(last_quarterly)
+        else:
+            last_quarterly = (this_quarterly - 1)//3 - 1
+            quarterlys.append(last_quarterly)
         for rec in self:
 
             kra = self.env['employee.kra'].search([('employee_id', '=', rec.id), ('state', '=', 'done'), ('year', '=', str(year)), ('name', 'in', months)])
@@ -40,38 +51,64 @@ class payroll_kra(models.Model):
     def _kra_final_score_compute(self):
         this_month = datetime.now().month
         year = datetime.now().year
-
+        months = [3, 6, 9, 12]
         if this_month == 1:
             last_month = 12
             year = year - 1
         else:
             last_month = this_month - 1
 
+        this_quarterly = (this_month - 1) // 3 + 1
+        if ((this_quarterly - 1)//3 - 1) == 0:
+            last_quarterly = 1
+
+        else:
+            last_quarterly = (this_quarterly - 1)//3 - 1
+
+
         for emp in self:
-            final_score_this = 0
-            final_score_last = 0
-            this_records = 0
-            last_records = 0
+            final_score_this_month = 0
+            final_score_last_month = 0
+            final_score_this_quarterly = 0
+            final_score_last_quarterly = 0
+            this_month_records = 0
+            last_month_records = 0
+            this_quarterly_records = 0
+            last_quarterly_records = 0
 
             for kra_id in emp.kras:
 
                 questions = self.env['employee.kra.question'].search([('employee_kra_id', '=', kra_id.id)])
 
                 for question in questions:
-                    if kra_id.name == str(this_month) and kra_id.year.name == str(year):
-                        this_records += 1
-                        final_score_this = final_score_this + question.final_score
+                    if kra_id.name == str(this_month) and kra_id.year.name == str(year) :
 
-                    elif kra_id.name == str(last_month) and kra_id.year.name == str(year):
-                        last_records += 1
-                        final_score_last = final_score_last + question.final_score
+                        if this_month in months and kra_id.quarterly == str(this_quarterly):
+                            this_quarterly_records += 1
+                            final_score_this_quarterly = final_score_this_quarterly + question.final_score
+                        else:
+                            this_month_records += 1
+                            final_score_this_month = final_score_this_month + question.final_score
 
-            if last_records == 0:
-                last_records = 1
-            if this_records == 0:
-                this_records = 1
+                    elif last_month in months and kra_id.quarterly == str(last_quarterly):
+                        last_quarterly_records += 1
+                        final_score_last_quarterly = final_score_last_quarterly + question.final_score
+                    else:
+                        last_month_records += 1
+                        final_score_last_month = final_score_last_month + question.final_score
 
-            emp.kra_final_score_last_month = final_score_last / last_records
-            emp.kra_final_score_this_month = final_score_this / this_records
+            if last_month_records == 0:
+                last_month_records = 1
+            if this_month_records == 0:
+                this_month_records = 1
+            if last_quarterly_records == 0:
+                last_quarterly_records = 1
+            if this_quarterly_records == 0:
+                this_quarterly_records = 1
+
+            emp.kra_final_score_last_month = final_score_last_month / last_month_records
+            emp.kra_final_score_this_month = final_score_this_month / this_month_records
+            emp.kra_final_score_last_quarterly = final_score_last_quarterly / last_quarterly_records
+            emp.kra_final_score_this_quarterly = final_score_this_quarterly / this_quarterly_records
 
 
