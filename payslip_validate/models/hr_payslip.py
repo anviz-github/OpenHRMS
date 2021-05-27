86fc946494916c542579f9ecd37c12c24e104893
Anviz <54052406+anviz-github@users.noreply.github.com>
2021-05-27 18:06:17 +0800

    update

--- payslip_validate/models/hr_payslip.py
+++
@@ -1,8 +0,0 @@
-from odoo import models
-
-
-class Payslip(models.Model):
-    _name = "hr.payslip"
-    _inherit = ["hr.payslip", "tier.validation", "mail.thread"]
-    _state_from = ["draft", "verify", "to approve"]
-    _state_to = ["done", "approved"]
